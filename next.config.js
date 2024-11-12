a2acc7bf6025e375f73b9f626293d296825246d3
Alexandru Marcus <ax.magoner@gmail.com>
2024-11-12 20:26:02 +0200

    workflow change, other deployment changes

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/trackerctp",
+    output: "export", 
+    reactStrictMode: false,
+  };
+  
+module.exports = nextConfig;
