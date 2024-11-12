b065cf37b9bd0e90bed7c0b54e76e77d9de352a4
Marian Dumitriu <bluegalaxy40@yahoo.com>
2024-11-12 23:23:46 +0200

    new prediction, not finished

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tensorflow import keras
 import os
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_cors import CORS
 #peak_hours = {7, 8, 14, 15, 16, 17, 18}
 
@@ -11,7 +11,7 @@ hour_congestion_index = {
     "2": 0,
     "3": 0,
     "4": 0,
-    "5": 0,
+    "5": 1,
     "6": 3,
     "7": 7.75,
     "8": 8.75,
@@ -27,8 +27,8 @@ hour_congestion_index = {
     "18": 7,
     "19": 4,
     "20": 3,
-    "21": 0,
-    "22": 0,
+    "21": 3,
+    "22": 1,
     "23": 0
 }
 
@@ -39,13 +39,14 @@ def create_app():
     
     return app
 
-def load_model(trip_id):
+def load_models(trip_id):
     model_path = f"models/{trip_id}_model.keras"
-    if os.path.exists(model_path):
-        return keras.models.load_model(model_path)
-    return None
+    model_forward_path = f"models/{trip_id}_model_forward.keras"
+    if os.path.exists(model_path) and os.path.exists(model_forward_path):
+        return keras.models.load_model(model_path), keras.models.load_model(model_forward_path)
+    return None, None
 
-def predict_time_to_reach(model, d1, d2, current_datetime):
+def predict_time_to_reach(model, model_forward, d1, d2, current_datetime):
     if d1 is None or d2 is None:
         return 0
 
@@ -58,14 +59,23 @@ def predict_time_to_reach(model, d1, d2, current_datetime):
         current_traffic_index = max(0, current_traffic_index - 3)
     max_iter = 25
 
+    #go back two minutes at a time until we reach almost d1, and then use model_forward to
     while d2 > d1 and max_iter > 0:
         input_data = np.array([[d2, current_traffic_index]])
+        input_data_forward = np.array([[d1, current_traffic_index]])
         distance_2mins_ago = model.predict(input_data)[0][0]
-        if distance_2mins_ago < d1:
-            time_elapsed += (d2 - d1) / (d2 - distance_2mins_ago) * 120
-            break
+
+        if d1 < 2.5:
+            distance_d1_forward = model_forward.predict(input_data_forward)[0][0]
+            if distance_d1_forward >= d2:
+                time_elapsed += (d2 - d1) / (distance_d1_forward - d1) * 120
+                break
         else:
-            time_elapsed += 120
-            d2 = distance_2mins_ago
+            if distance_2mins_ago <= d1:
+                time_elapsed += (d2 - d1) / (d2 - distance_2mins_ago) * 120
+                break
+        
+        time_elapsed += 120
+        d2 = distance_2mins_ago
         max_iter -= 1
     return time_elapsed
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from main import load_model, predict_time_to_reach
+from main import load_models, predict_time_to_reach
 from flask import Blueprint, request, jsonify
 
 routing_profile = Blueprint('routing_profile', __name__)
@@ -10,12 +10,12 @@ def predict():
     trip_id = data['trip_id']
     d1 = data['d1']
     d2 = data['d2']
-    model = load_model(trip_id)
-    if model is None:
+    model, model_forward = load_models(trip_id)
+    if model is None or model_forward is None:
         return jsonify({"error": "Model not found."}), 404
 
     current_datetime = datetime.now()
-    predicted_time = float(predict_time_to_reach(model, d1, d2, current_datetime))
+    predicted_time = float(predict_time_to_reach(model, model_forward, d1, d2, current_datetime))
     
     return jsonify({"predicted_time": predicted_time})
 
